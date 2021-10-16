78a3e2191b36ef1285c783971d49c2ba61a2f5aa
Rohinikhosla111 <>
2021-10-16 18:21:30 +0530

    Final remote commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,8 +8,19 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-        //Creating Card class object
+       //Creating Card class object
        Card card = new Card();
+       
+       //Setting values for Card class object
+       card.setSuit("Hearts");
+       card.setValue(1);
+       
+       //Printing Card class object values
+       System.out.println("Suit : " + card.getSuit());
+       System.out.println("Value : " + card.getValue());
+       
+       //Creating another Card class object
+       Card card1 = new Card();
     }
     
 }
