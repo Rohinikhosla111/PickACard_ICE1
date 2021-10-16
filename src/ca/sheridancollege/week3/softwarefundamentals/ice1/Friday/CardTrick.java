4ffd4bf508efa3e793fae9563cfb0ef5ca33035d
Rohinikhosla111 <>
2021-10-16 18:04:53 +0530

    First remote commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,12 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Rohini Khosla - 991625011
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card card = new Card();
     }
     
 }
